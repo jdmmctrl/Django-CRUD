565995901a7bcf79cabfa531541feadfa127b757
jdmmctrl <juandavidmurillomejia0718@outlook.es>
2023-04-07 22:25:02 -0500

    Actualizacion 07/04/2023

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Task
+
+
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('title', 'user', 'created', 'datecompleted', 'important')
+    list_filter = ('user', 'created', 'datecompleted', 'important')
+    search_fields = ('title', 'description')
+    readonly_fields = ('created', 'datecompleted')
+
 
 # Register your models here.
+admin.site.register(Task, TaskAdmin)
