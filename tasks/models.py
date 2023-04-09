437d7785c0e2ed7e93e74c1334eeeec90d247943
jdmmctrl <juandavidmurillomejia0718@outlook.es>
2023-04-08 21:54:04 -0500

    Actualizacion 08/04/2023

--- tasks/models.py
+++ tasks/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 # Create your models here.
 
 
@@ -13,4 +14,4 @@ class Task(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.title + ' by ' + self.user.username
+        return self.title + ' - by ' + self.user.username
--- tasks/views.py
+++ tasks/views.py
@@ -3,7 +3,8 @@ from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 from django.contrib.auth import login, logout, authenticate
 from django.db import IntegrityError
-
+from .forms import TaskForm
+from .models import Task
 
 # Create your views here.
 
@@ -38,7 +39,29 @@ def signup(request):
 
 
 def tasks(request):
-    return render(request, 'tasks.html')
+    tasks = Task.objects.filter(user=request.user, datecompleted__isnull=True)
+    return render(request, 'tasks.html', {"tasks": tasks})
+
+
+def create_task(request):
+
+    if request.method == 'GET':
+        return render(request, 'create_task.html', {
+            'form': TaskForm
+        })
+
+    else:
+        try:
+            form = TaskForm(request.POST)
+            new_task = form.save(commit=False)
+            new_task.user = request.user
+            new_task.save()
+            return redirect('tasks')
+        except ValueError:
+            return render(request, 'create_task.html', {
+                'form': TaskForm,
+                'error': 'Something went wrong'
+            })
 
 
 def signout(request):
